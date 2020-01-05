bb41890f5d1df80cc77e8b073f55209b45523347
rickhodder <rickhodder@msn.com>
2020-01-05 17:07:54 -0500

    Initial setup of solution, with intrastructure and core

---
+++ src/Infrastructure/MessageProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Interfaces;
+
+namespace Infrastructure
+{
+    public class MessageProvider:IMessageProvider
+    {
+        public string GetMessage()
+        {
+            return this.ToString();
+        }
+    }
+}
---
+++ src/UI/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMessageProvider _messageProvider;
+
+        public HomeController(ILogger<HomeController> logger, IMessageProvider messageProvider)
+        {
+            _logger = logger;
+            _messageProvider = messageProvider;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["message"]= _messageProvider.GetMessage();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
